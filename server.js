fcb18e4ebaf1246bb187e6680d520f43a30be729
Graham Patton <graham.m.patton@gmail.com>
2022-05-28 13:00:06 -0400

    setting up index and more express

--- server.js
+++ server.js
@@ -2,7 +2,16 @@
 
 const express = require('express');
 const app = express();
+
 app.listen(3000, function() {
     console.log('listening on 3000')
-    })
-    
+    });
+    //check by running node server.js
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+//     })
+    //run node server.js
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
