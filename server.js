ff07967e28b00946c905e6f9957939f7311c9f1d
Graham Patton <graham.m.patton@gmail.com>
2022-05-28 12:52:36 -0400

    express set up

---
+++ server.js
@@ -0,0 +1,8 @@
+// console.log('may the node be with you')
+
+const express = require('express');
+const app = express();
+app.listen(3000, function() {
+    console.log('listening on 3000')
+    })
+    
