e3733ee92b0c01a99cb373bb4bf4bd2130f8cdc5
Graham Patton <graham.m.patton@gmail.com>
2022-05-31 17:25:56 -0400

    adding in ability to insert quotes to html

--- server.js
+++ server.js
@@ -5,10 +5,40 @@ const bodyParser= require('body-parser');
 const app = express();
 
 const MongoClient = require('mongodb').MongoClient
+
 MongoClient.connect('mongodb+srv://grahammpatton:1422Scales>@cluster0.5ejb1.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true
 })
 .then(client => {
 console.log('Connected to Database')
+const db = client.db('star-wars-quotes')
+const quotesCollection = db.collection('quotes')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray()
+    .then(results => {
+        res.render('index.ejs', { quotes: results })
+        })
+        
+    .catch(error => console.error(error))
+    // ...
+    res.render('index.ejs', {})
+    })
+    
+app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+    .then(result => {
+    res.redirect('/')
+    })
+    .catch(error => console.error(error))
+    })
+    
+app.listen(3000, function() {
+    console.log('listening on 3000')
+    });
+    //check by running node server.js
+app.set('view engine', 'ejs')
+
 })
 .catch(error => console.error(error))
 
@@ -23,21 +53,5 @@ console.log('Connected to Database')
 //   client.close();
 // });
 
-app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-    });
-    //check by running node server.js
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World')
-//     })
-    //run node server.js
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
 
